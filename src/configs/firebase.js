8248d720536afd883e249b86dd9cefb7464f140c
Abdullah Al Yeamin Maruf Prionto <34392226+PriontoAbdullah@users.noreply.github.com>
2022-04-21 11:54:27 +0600

    Update firebase.js

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'AIzaSyDteAAYy44fkI5CPzQfBBmEiqddJCpRnJs',
-  authDomain: 'anushilon-bd.firebaseapp.com',
+  apiKey: '',
+  authDomain: '',
   databaseURL:
-    'https://anushilon-bd-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'anushilon-bd',
-  storageBucket: 'anushilon-bd.appspot.com',
-  messagingSenderId: '659987306508',
-  appId: '1:659987306508:web:16cf501fdde606f1fab629',
-  measurementId: 'G-MZGERKL5LF',
+    '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 });
 
 const db = firebaseApp.firestore();
